039feaee46e7f1edbb2c872b965295a1892088a5
Susan <suzutoyuki@gmail.com>
2018-02-12 01:59:15 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ function init() {
 
 let index=0
 
-addEventListener('keydown,function(e)'){
+addEventListener('keydown',function(e)){
   const key = parseInt(e.detail || e.which);
 
   if (key === alphabet[index]) {
