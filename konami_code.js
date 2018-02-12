178e7f3e546e0711d4c72de05f4948fe292731f8
Susan <suzutoyuki@gmail.com>
2018-02-12 01:58:22 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,9 +2,23 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-code.addEventListener('click, function(event){alert('I was clicked')}');
 
-  for (let i = 0, l = code.length; i < l; i++) {
-    triggerKeyDown(code[i])
+let index=0
+
+addEventListener('keydown,function(e)')
+  const key = parseInt(e.detail || e.which);
+
+  if (key === alphabet[index]) {
+    index++;
+
+    if (index === alphabet.length) {
+      alert("Hurray!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
   }
 }
