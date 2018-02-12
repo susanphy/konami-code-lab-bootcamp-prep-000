27f4f3dd2e49cea4c6a7bfc7f11d7d8228bc1af4
Susan <suzutoyuki@gmail.com>
2018-02-12 02:02:20 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
 
 document.body.addEventListener('keydown',function(e)){
-  const key = parseInt(e.detail || e.which)
+var key = parseInt(e.detail || e.which)
 
   if (key === code[index]) {
     index++;
